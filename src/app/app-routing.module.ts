5a79437f17c06e46a492249e071e9e556accb5b6
Pinto Júnior <junior_pinto@hotmail.com>
2022-04-01 14:39:16 -0300

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './page/about/about.component';
+import { SkillsComponent } from './page/skills/skills.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '', 
+    component: AboutComponent,
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,28 @@ import { HeaderComponent } from './template/header/header.component';
 
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from './template/footer/footer.component';
-import { AboutComponent } from './page/about/about.component'
+import { AboutComponent } from './page/about/about.component';
+import { SkillsComponent } from './page/skills/skills.component';
+import { NavComponent } from './template/nav/nav.component';
+
+// import {MatSidenavModule} from 'angular/material/sidenav';
+// import {MatListModule} from 'angular/material/list'
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    AboutComponent
+    AboutComponent,
+    SkillsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    // MatSidenavModule,
+    // MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]

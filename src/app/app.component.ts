e88d3d8b6f221e0c22ad937e83d7e1a37cc23540
Pinto Júnior <junior_pinto@hotmail.com>
2022-04-04 18:19:10 -0300

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,10 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   
   ngAfterViewInit(): void {
-    throw new Error('Method not implemented.');
+    
   }
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    
   }
   title = 'portfolio';
   toggleSidenav(): void {
